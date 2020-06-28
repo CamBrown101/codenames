082a9b172cec200d149ed8a93ba076b103362912
CamBrown101 <cameron-brown@live.com>
2020-06-28 14:32:05 -0700

    latest update

---
+++ index.js
@@ -0,0 +1,28 @@
+let nmbguess = 0;
+let hint = '';
+$(document).ready(function () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+  });
+  //Hint Submission
+  $('.submit-btn').click(function () {
+    $('.submit-btn').hide('.submit-btn');
+    // update variables with user inputs
+    nmbguess = $('.number-guess').val();
+    hint = $('.hint').val();
+    let html = `<h1 class="inputs">${hint} ${nmbguess}</h1>`;
+    //Hide Input Bars
+    $('.hint').hide();
+    $('.number-guess').hide();
+    //Display Hints
+    $('.hint-submission').append(html);
+    //Reset Inputs
+    $('.hint').val('');
+    $('.number-guess').val(1);
+    console.log(nmbguess);
+    console.log(hint);
+  });
+  $('.burger').on('click', function () {
+    $('.');
+  });
+});
